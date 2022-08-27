d31555b313f8239f860a73e946c213c2204f1d1e
Cindy <cindychynoweth@gmail.com>
2022-08-27 16:23:24 -0600

    cinderbease

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Express.js-Note-Taker
+
+## Table of Contents
+
+1. [Description](#description)
+2. [Resources](#resources)
+3. [Visuals](#visuals)
+4. [Author and Acknowledgements](#author-and-acknowledgements)
+
+## Decription
+
+The assignement requires students to use existing starter code to create an application that can be used to write and save notes. This application will use an Express.js back end. The application will save and retrieve data from a JSON file. It is my task to code the back end, connect the two, and then deploy the entire application to Heroku.
+
+## Resources
+
+-[LIVE SITE]()
+
+-[Repository](https://github.com/Cinderbeast/Express.js-Note-Taker)
+
+- [Heroku](https://cinderbeast-app.herokuapp.com/)
+ https://git.heroku.com/cinderbeast-app.git
+
+## Visuals
+
+![Note Taker Screenshot]()
+
+## Author and Acknowledgements
+
+Code created by University of Utah Bootcamp, edited by Cindy
+
+## Bonus
+
+You haven’t learned how to handle DELETE requests, but this application offers that functionality on the front end. As a bonus, try to add the DELETE route to the application using the following guideline:
+
+* `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
---
+++ server.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const api = require("")
+const api = require("./public/assets/js/index.js")
+
+const database = require('./db/notes.json')
+const { uuid } = require('./utils/id.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// GET Route from Homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public.index.html'));
+});
+
+// GET Route for Notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for retrieving all the Notes
+app.get('/api/notes', (req, res) => {
+
+  fs.readFile(path.join(__dirname, './db/notes.json'), 'utf-8', (error, data) => {
+    if (error) throw error;
+    res.json(JSON.parse(data));
+  });
+});
+
+// POST Route for a new Note
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const createdNote = { title, text, id: uuid(), };
+    database.push(createdNote);
+
+    let storedNotes = JSON.stringify((database), null, 2);
+
+    fs.writeFile(`./db/notes.json`, storedNotes, () => {
+      const response = {
+        body: createdNote,
+      }
+      res.json(response);
+    })
+  };;
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
